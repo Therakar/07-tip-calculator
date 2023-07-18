bc3dc581c0ab8fd99a951e77a7e46a7b49c97eba
Therakar <matteo.pau200@gmail.com>
2023-07-19 00:03:10 +0200

    passes props

--- src/components/TipCalculator.js
+++ src/components/TipCalculator.js
@@ -2,15 +2,37 @@ import Bill from "./Bill";
 import TipPercentage from "./TipPercentage";
 import Output from "./Output";
 import ResetBtn from "./ResetBtn";
+import { useState } from "react";
 
 export default function TipCalculator() {
+  //Pieces of state
+  const [bill, setBill] = useState("");
+  const [userPercentage, setUserPercentage] = useState(0);
+  const [friendPercentage, setFriendPercentage] = useState(0);
+
+  const tip = bill * ((userPercentage + friendPercentage) / 2 / 100);
+
+  //handler functions
+  function handleReset() {
+    setBill("");
+    setUserPercentage(0);
+    setFriendPercentage(0);
+  }
+
   return (
     <div>
-      <Bill />
-      <TipPercentage>How did you like the service?</TipPercentage>
-      <TipPercentage>How did your friend like the service?</TipPercentage>
-      <Output />
-      <ResetBtn />
+      <Bill bill={bill} onSetBill={setBill} />
+      <TipPercentage percentage={userPercentage} onSelect={setUserPercentage}>
+        How did you like the service?
+      </TipPercentage>
+      <TipPercentage
+        percentage={friendPercentage}
+        onSelect={setFriendPercentage}
+      >
+        How did your friend like the service?
+      </TipPercentage>
+      <Output bill={bill} tip={tip} />
+      <ResetBtn onReset={handleReset} />
     </div>
   );
 }
