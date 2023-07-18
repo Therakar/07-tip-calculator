cddf34db8dcefb813c292b8124c74b080aa1d1a0
Therakar <matteo.pau200@gmail.com>
2023-07-18 23:15:36 +0200

    TipPercentage options

--- src/components/TipPercentage.js
+++ src/components/TipPercentage.js
@@ -2,7 +2,12 @@ export default function TipPercentage({ children }) {
   return (
     <div>
       <label>{children}</label>
-      <select></select>
+      <select>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="15">It was good (10%)</option>
+        <option value="20">It was Perfect! (20%)</option>
+      </select>
     </div>
   );
 }
